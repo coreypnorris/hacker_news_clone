c35341711c55d7364f7309ad6dde74e434e3af84
Corey Norris <coreypnorris@gmail.com>
2014-06-12 14:54:23 -0700

    add user authentication via Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 HackerNewsClone::Application.routes.draw do
+  devise_for :user, :path => '', :path_names => { :sign_in => "sign_in", :sign_out => "sign_out", :sign_up => "register" }
   resources :posts, only: [:index, :show]
 
   root to: "posts#index"
