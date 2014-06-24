487c4427356fdba85652658c7175756adc59d572
Corey Norris <coreypnorris@gmail.com>
2014-06-24 10:20:08 -0700

    create user_posts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 HackerNewsClone::Application.routes.draw do
   devise_for :user, :path => '', :path_names => { :sign_in => "sign_in", :sign_out => "sign_out", :sign_up => "register" }
-  resources :posts, only: [:index, :show]
+  resources :user, only: [:show] do
+    resources :posts
+  end
+  resources :posts, only: [:index]
 
   root to: "posts#index"
 end
