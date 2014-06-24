b93f3753445fcfd7569b9938079e7c03ed582eef
Corey Norris <coreypnorris@gmail.com>
2014-06-24 11:12:00 -0700

    create post_type method to determine if post is a question or url

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,13 @@ class Post < ActiveRecord::Base
   validates_presence_of :question, :unless => :url?
 
   belongs_to :user
+
+  def post_type
+    if question.strip.length == 0
+      return "url"
+    elsif url.strip.length == 0
+      return "question"
+    end
+  end
+
 end
