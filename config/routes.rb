3d33dd87623fdf6a55d908eccb811c682ec2481d
Corey Norris <coreypnorris@gmail.com>
2014-06-30 14:57:00 -0700

    create comment routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ HackerNewsClone::Application.routes.draw do
   devise_for :user, :path => '', :path_names => { :sign_in => "sign_in", :sign_out => "sign_out", :sign_up => "register" }
   resources :user, only: [:show] do
     resources :posts
+    resources :comments, only: [:index]
   end
   resources :posts, only: [:index, :show] do
     resources :votes, only: [:new, :create]
+    resources :comments, only: [:create]
+  end
+
+  resources :comments, only: [:create] do
+    resources :comments, only: [:create]
   end
 
   root to: "posts#index"
