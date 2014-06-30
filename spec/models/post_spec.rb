6bb2199918e540320aa378a80c1c9c542173f8f6
Corey Norris <coreypnorris@gmail.com>
2014-06-30 06:47:16 -0700

    refactor rspec syntax on unit test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Post do
+RSpec.describe Post do
   it { should validate_presence_of :title }
   it { should validate_presence_of :url or :question }
   it { should belong_to :user }
