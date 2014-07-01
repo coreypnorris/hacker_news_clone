43a2629eefdb78c9845bcace970190f576d0f24a
Corey Norris <coreypnorris@gmail.com>
2014-07-01 13:37:40 -0700

    create and apply all_comments method

--- app/models/post.rb
+++ app/models/post.rb
@@ -56,6 +56,12 @@ class Post < ActiveRecord::Base
       return "#{votes.count} points"
     end
   end
+
+  def all_comments
+    parent_comments = Comment.where(:commentable_id => id)
+    child_comments = Comment.where(:commentable_id => parent_comments.map(&:id))
+    parent_comments.count + child_comments.count
+  end
 end
 
 
