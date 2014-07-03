4e13861315eb7669d09798d3a23e7ad614e82aba
Corey Norris <coreypnorris@gmail.com>
2014-07-03 13:45:58 -0700

    create comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,53 +1,30 @@
 class CommentsController < ApplicationController
-  before_filter :authenticate_user!, except: [:index, :show]
-
-  def index
-    @comments = Comment.find(:all, :include => :votes, :order => "votes_count DESC")
-  end
 
   def new
-    @comment = Comment.new
+    @comment = Comment.new(:parent_id => params[:parent_id])
   end
 
   def create
-    @comment = Comment.new comment_params
+    @parent = Post.find(params[:post_id]) if params[:post_id]
+    @parent = Comment.find(params[:comment][:parent_id]) if params[:comment][:parent_id]
+    @post = @parent.class == Post ? @parent : @parent.commentable
+    @comment = Comment.build_from( @post, current_user.id, comment_params[:body] )
     if @comment.save
-      current_user.comments << @comment
       flash[:notice] = "Your comment has been submitted."
-      redirect_to root_url
+      if params[:post_id]
+        redirect_to post_path(@parent)
+      elsif params[:comment][:parent_id]
+        @comment.move_to_child_of(@parent)
+        redirect_to post_path(@parent.commentable)
+      end
     else
-      flash[:alert] = "Something went wrong. Please try to save your comment again."
-      render 'new'
+      flash[:error] = "Something went wrong. Please try to save your comment again."
+      redirect_to :back
     end
-  end
-
-  def show
-    @comment = Comment.find(params[:id])
-  end
-
-  def edit
-    @comment = Comment.find(params[:id])
-  end
-
-  def update
-    @comment = Comment.find(params[:id])
-    if @comment.update comment_params
-      flash[:notice] = "Your comment has been changed."
-      redirect_to comment_path @comment
-    else
-      render 'edit'
-    end
-  end
-
-  def destroy
-    @comment = Comment.find(params[:id])
-    @comment.destroy
-    flash[:alert] = "Your comment has been removed."
-    redirect_to comments_path
   end
 
 private
   def comment_params
-    params.require(:comment).permit(:body, :user_id)
+    params.require(:comment).permit(:body)
   end
 end
