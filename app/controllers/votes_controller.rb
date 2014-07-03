c28cf7d2ff48238c0c611f3d4f2798ff2a49eb86
Corey Norris <coreypnorris@gmail.com>
2014-07-03 16:50:10 -0700

    refactor votes_controller for comments

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,8 +1,9 @@
 class VotesController < ApplicationController
   def new
     @vote = Vote.new
-    @post = Post.find(params[:post_id])
-    @post.votes << @vote
+    @parent = Post.find(params[:post_id]) if params[:post_id]
+    @parent = Comment.find(params[:comment_id]) if params[:comment_id]
+    @parent.votes << @vote
     current_user.votes << @vote
     redirect_to :back
   end
