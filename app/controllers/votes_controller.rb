d706c596af6cfbe4dabef40b8c2a60cebb9c155f
Corey Norris <coreypnorris@gmail.com>
2014-07-04 11:10:59 -0700

    refactor new action for polymorphic comments undefined method user_name bug

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,10 +1,14 @@
 class VotesController < ApplicationController
   def new
-    @vote = Vote.new
-    @parent = Post.find(params[:post_id]) if params[:post_id]
-    @parent = Comment.find(params[:comment_id]) if params[:comment_id]
-    @parent.votes << @vote
-    current_user.votes << @vote
-    redirect_to :back
+    @vote = Post.find(params[:post_id]).votes.new if params[:post_id]
+    @vote = Comment.find(params[:comment_id]).votes.new if params[:comment_id]
+    if @vote.save
+      @vote.user = current_user
+      flash[:notice] = "Your vote has been submitted."
+      redirect_to :back
+    else
+      flash[:alert] = "Something went wrong. Please try to save your post again."
+      redirect_to :back
+    end
   end
 end
