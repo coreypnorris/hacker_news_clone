ff0b4411350b399672f5fd0b4d8c6cafd38c7b6f
Corey Norris <coreypnorris@gmail.com>
2014-07-06 18:02:15 -0700

    fix bug: pluralize user resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 HackerNewsClone::Application.routes.draw do
   devise_for :user, :path => '', :path_names => { :sign_in => "sign_in", :sign_out => "sign_out", :sign_up => "register" }
-  resources :user, only: [:show] do
+  resources :users, only: [:show] do
     resources :posts
     resources :comments, only: [:index]
   end
