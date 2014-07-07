314f9a1b04691b36afc5b0590c54f0e13a07b9a1
Corey Norris <coreypnorris@gmail.com>
2014-07-06 17:19:26 -0700

    create show method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,6 +24,10 @@ class CommentsController < ApplicationController
     end
   end
 
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
 private
   def comment_params
     params.require(:comment).permit(:body)
